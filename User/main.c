a75f30f48780efc53246288bc1e7a50a5299c051
CetaceanBored <cxqbored@gmail.com>
2025-01-12 11:48:27 +0800

    Update Serial

--- User/main.c
+++ User/main.c
@@ -4,38 +4,31 @@
 #include "Serial.h"
 #include "OLED.h"
 #include <stdint.h>
-
+#include <string.h>
+#include <stdlib.h>
 
 int main(void)
 {
 	Delay_init();
 	OLED_Init();
 	Serial_UASRT2_init();
-	//PWM_Init();
+	PWM_Init();
 	while (1)
 	{
 		OLED_ShowString(6, 2, "Testing...", 6);
-		/*
-		for (uint8_t i = 10; i <= 170; i+=5)
-		{
-			OLED_ShowNum(0, 0, i, 3, 8);
-			PWM_Set(1, (i / 180.0) * 1000 + 250);
-			Delay_ms(200);
-		}
-		for (uint8_t i = 170; i >= 10; i-=5)
-		{
-			OLED_ShowNum(0, 0, i, 3, 8);
-			PWM_Set(1, (i / 180.0) * 1000 + 250);
-			Delay_ms(200);
-		}
-		*/
 		if (Serial_RxFlag) {
-			
-			OLED_ShowString(1, 0, Serial_Packet, 8);
+			//OLED_ShowString(1, 0, Serial_Packet, 6);
+			char str1[10], str2[10];
+			uint16_t val1, val2;
+			strncpy(str1, Serial_Packet, 4);
+			strncpy(str2, Serial_Packet + 4, 4);
+			str1[4] = '\0', str2[4] = '\0';
+			val1 = atoi(str1), val2 = atoi(str2);
+			OLED_ShowNum(0, 0, val1, 4, 6);
+			OLED_ShowNum(0, 30, val2, 4, 6);
+			PWM_Set(1, val1);
+			PWM_Set(2, val2);
 			Serial_RxFlag = 0;
-		}
-		
-
-		
+		}	
 	}
 }
