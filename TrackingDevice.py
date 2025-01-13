089cc24473a061f8be5de98d147ff48d9e6de2d9
CetaceanBored <wuliao_bored@163.com>
2025-01-13 17:47:35 +0800

    DetectPencilContours

--- TrackingDevice.py
+++ TrackingDevice.py
@@ -9,10 +9,24 @@ cam.start()
 
 cv2.namedWindow("Camera", cv2.WINDOW_AUTOSIZE)
 
+min_area = 3000
+
 while True:
     frame = cv2.cvtColor(cam.capture_array(), cv2.COLOR_RGB2BGR)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray, (5, 5), 0)
+    thresh = cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2)
+    contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    
+    for contour in contours:
+        epsilon = 0.02 * cv2.arcLength(contour, True)
+        approx = cv2.approxPolyDP(contour, epsilon, True)
 
+        if len(approx) == 4 and cv2.contourArea(approx) > min_area:
+            cv2.drawContours(frame, [approx], -1, (0, 0, 255), 2)
+    
     cv2.imshow("Camera", frame)
+    # cv2.imshow("TEST", thresh)
     if(cv2.waitKey(1) & 0xFF == ord('q')):
         break
 
